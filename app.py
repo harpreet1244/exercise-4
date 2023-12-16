8f695895c6d4312d6a924d2d7f60b34782546f37
Harpreet Singh <134891482+harpreet1244@users.noreply.github.com>
2023-12-16 14:25:44 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+f = "harp"
+g = "eet"
+full = f" {len(f)} {5+5}"
+print(full))
+
+
+
+
+course = "   programming"
+print(course.upper())
+print(course.lower())
+print(course.strip())
+print(course.find("pro"))
+print("pro" in course)
+ 
+
+x = 10
+x +=3
+#x = x + 3
+print(x)
