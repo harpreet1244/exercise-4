a35ee9ae0851405522fbd2466fd79d97a49e9a78
Harpreet Singh <134891482+harpreet1244@users.noreply.github.com>
2024-04-09 00:02:03 -0600

    Add files via upload

---
+++ prac day 3.py
@@ -0,0 +1,16 @@
+
+x = float(input("whats x ? "))
+y = float(input("whats y ? "))
+z = round(x + y)
+print(z)
+
+# just to put a comma to make it money number
+x = float(input("whats x ? "))
+y = float(input("whats y ? "))
+z = round(x + y)
+print(f"{z: ,}")
+
+x = float(input("whats x ? "))
+y = float(input("whats y ? "))
+z = x / y
+print(f"{z:.2f}")
