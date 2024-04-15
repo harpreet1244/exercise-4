64cbfb755490ee9e1c08e8b93ab7c1fade3dc3cb
Harpreet Singh <134891482+harpreet1244@users.noreply.github.com>
2024-04-15 07:52:55 -0600

    Add files via upload

---
+++ #def function.py
@@ -0,0 +1,30 @@
+# def function
+
+def hello(to):
+
+    print("hello", to)
+
+
+name = input("enter the name ? ")
+hello(name)
+
+
+def bro(who):
+
+    print("hello,", who)
+
+
+name = input("enter the name")
+bro(name)
+
+
+def main():
+    name = input("enter the name")
+    hello(name)
+
+
+def hello(name):
+    print("hello", name)
+
+
+main()
