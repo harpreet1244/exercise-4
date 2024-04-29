aece4c188e9030932644343cc78248d88359bc96
Harpreet Singh <134891482+harpreet1244@users.noreply.github.com>
2024-04-28 22:42:55 -0600

    Add files via upload

---
+++ function prac.py
@@ -0,0 +1,24 @@
+def main():
+    name = input("enter the student name:- ")
+    print("there is the result of ", name)
+    result()
+
+
+def result():
+    print("english = 92")
+    print("punjabi = 99")
+
+
+main()
+
+
+def main():
+    name = input("enter the name ")
+    hello(name)
+
+
+def hello(who):
+    print("hello", who)
+
+
+main()
