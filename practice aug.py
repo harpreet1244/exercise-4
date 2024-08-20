b2810e4fda73607e0f385054f97abe85ef1a7855
Harpreet Singh <134891482+harpreet1244@users.noreply.github.com>
2024-08-20 14:33:00 -0600

    Add files via upload

---
+++ practice aug.py
@@ -0,0 +1,19 @@
+"""
+x = int(input("enter the number: "))
+y = int(input("enter the next number: "))
+
+print(x + y)
+"""
+"""
+x = float(input("enter the number: "))
+y = float(input("enter the next number: "))
+z = round(x / y, 2)
+
+print(z)
+
+print(f"{z: .2f}")
+"""
+
+name = (input("enter the name: ")).upper()
+
+print(f"hello {name}, sorry for delay in the delivery")
